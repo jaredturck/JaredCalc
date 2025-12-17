99d5592c107e4a27ca55d13ae54804c99ee3eaa2
Jared Turck <jaredturck8@gmail.com>
2025-12-17 21:53:40 +0000

    Added functions file

---
+++ functions.py
@@ -0,0 +1,71 @@
+import math
+
+topList = {
+    'abs': op.absolute,
+    'arg': op.argument,
+    'conj': op.conjugate,
+    'Im': op.imPart,
+    'Re': op.realPart,
+    'sgn': op.signum,
+    'sin': op.sin,
+    'cosec': op.csc,
+    'csc': op.csc,
+    'cos': math.cos,
+    'sec': op.sec,
+    'tan': op.tan,
+    'cot': op.cot,
+    'sinh': op.sinh,
+    'cosh': op.cosh,
+    'tanh': op.tanh,
+    'asin': op.arcsin,
+    'arcsin': op.arcsin,
+    'acos': op.arccos,
+    'arccos': op.arccos,
+    'atan': op.arctan,
+    'arctan': op.arctan,
+    'sqrt': op.sqrt,
+    'ln': op.ln,
+    'lg': op.lg,
+    'normcdf': op.normcdf,
+    'normpdf': op.normpdf,
+    'invnorm': op.invnorm,
+}
+
+symbols = '''
++
+-
+*
+/
+//
+%
+^
+!
+P
+C
+=
+=>
+&&
+||
+<
+<=
+>
+>=
+==
+!=
+<*
+<==
+>*
+>==
+===
+!==
+?
+:
+@
+$
+.
+><
+</
+/>
+<+>
+;
+'''
