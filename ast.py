de54694d09abb5e1e4b77a52c0cd1f53aed61b04
Jared Turck <jaredturck8@gmail.com>
2025-12-17 23:36:14 +0000

    Added in factorial

--- ast.py
+++ ast.py
@@ -1,4 +1,4 @@
-import re, decimal
+import re, decimal, math
 from functions import function_list, constants
 
 class Parse:
@@ -6,21 +6,21 @@ class Parse:
         self.tokens = self.tokenize(expression)
         self.i = 0
         self.ast = self.expr()
-    
+
     def tokenize(self, expression):
         ''' Tokenize the input expression into numbers, operators, and parentheses '''
-        return re.findall(r'\d+\.\d+|\d+|[a-zA-Z]+|[+*/()-]', expression)
-    
+        return re.findall(r'\d+\.\d+|\d+|[a-zA-Z]+|[+*/()!\-]', expression)
+
     def peek(self):
         ''' Look at the next token without consuming it '''
         return self.tokens[self.i] if self.i < len(self.tokens) else None
-    
+
     def eat(self):
         ''' Consume and return the next token '''
         t = self.peek()
         self.i += 1
         return t
-    
+
     def _exp(self, symbols, func):
         ''' Generic expression parser for left-associative binary operations '''
         node = func()
@@ -28,15 +28,23 @@ class Parse:
             op = self.eat()
             node = ('symbol', op, node, func())
         return node
-    
+
     def expr(self):
         ''' Parse add and subtract '''
         return self._exp(('+', '-'), self.term)
-    
+
     def term(self):
         ''' Parse multiply and divide '''
-        return self._exp(('*', '/'), self.factor)
-    
+        return self._exp(('*', '/'), self.postfix)
+
+    def postfix(self):
+        ''' Parse postfix operators like factorial '''
+        node = self.factor()
+        while self.peek() == '!':
+            self.eat()
+            node = ('postfix', '!', node)
+        return node
+
     def factor(self):
         ''' Parse numbers and parenthesized '''
         t = self.eat()
@@ -44,34 +52,44 @@ class Parse:
             n = self.expr()
             self.eat()
             return n
-        
+
         # check for function
         if t in function_list:
             self.eat()
             arg = self.expr()
             self.eat()
             return ('function', t, arg)
-        
+
         # check for constant
         if t in constants:
             return ('num', constants[t])
-        
+
         # must be a number
         if '.' in t:
             return ('num', decimal.Decimal(t))
         return ('num', int(t))
-    
+
     def evaluate(self, node=None):
         ''' Evaluate the AST recursively '''
         if not node:
             node = self.ast
+
         if node[0] == 'num':
             return node[1]
+
         if node[0] == 'function':
             _, name, arg = node
             val = self.evaluate(arg)
             return function_list[name](val)
-        
+
+        if node[0] == 'postfix':
+            _, op, inner = node
+            a = self.evaluate(inner)
+            if op == '!':
+                if isinstance(a, decimal.Decimal):
+                    a = int(a)
+                return math.factorial(a)
+
         _, op, left, right = node
         a = self.evaluate(left)
         b = self.evaluate(right)
@@ -84,7 +102,7 @@ class Parse:
             return a * b
         elif op == '/':
             return a / b
-    
+
     def __str__(self):
         v = self.evaluate()
         if isinstance(v, float):
