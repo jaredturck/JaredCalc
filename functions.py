d503f599cd353945fa08648bd8b3e4530294478d
Jared Turck <jaredturck8@gmail.com>
2025-12-18 12:32:02 +0000

    Added sin

--- functions.py
+++ functions.py
@@ -1,7 +1,59 @@
-import math
+# import math
 import cmath
 import statistics
 import operator
+import time
+
+def timeit(fn):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = fn(*args, **kwargs)
+        end = time.time()
+        print(f"Time taken: {end - start:.10f} seconds")
+        return result
+
+    return wrapper
+
+class Func:
+    PI = 3.141592653589793
+    E = 2.718281828459045
+    TAU = 6.283185307179586
+    HALF_PI = 1.5707963267948966
+    LN1_1 = 0.09531017980432493
+    LN2 = 0.6931471805599453
+    LN10 = 2.302585092994046
+    SQRT2 = 1.4142135623730951
+    SQRT_2PI = 2.5066282746310002
+
+    @staticmethod
+    def sinFn(x):
+        ''' Compute sine using Taylor series expansion '''
+        
+        # Reduce to range [-π, π]
+        if abs(x) > Func.PI:
+            x = ((x + Func.PI) % (Func.TAU)) - Func.PI
+
+        # Reduce to range [-π/2, π/2]
+        if x > Func.HALF_PI:
+            x = Func.PI - x
+        elif x < -Func.HALF_PI:
+            x = -Func.PI - x
+
+        s = 0.0
+        term = x
+        n = 1
+        x2 = x * x
+
+        while True:
+            p = s
+            s += term
+            if s == p:
+                break
+
+            term *= -x2 / ((n + 1) * (n + 2))
+            n += 2
+
+        return s
 
 _normal = statistics.NormalDist()
 
@@ -12,75 +64,75 @@ function_list = {
     "Im": operator.attrgetter("imag"),
     "Re": operator.attrgetter("real"),
     "sgn": lambda x: 0 if x == 0 else (1 if x > 0 else -1),
-    "sin": math.sin,
-    "cosec": lambda x: 1 / math.sin(x),
-    "csc": lambda x: 1 / math.sin(x),
-    "cos": math.cos,
-    "sec": lambda x: 1 / math.cos(x),
-    "tan": math.tan,
-    "cot": lambda x: 1 / math.tan(x),
-    "sinh": math.sinh,
-    "cosh": math.cosh,
-    "tanh": math.tanh,
-    "asin": math.asin,
-    "arcsin": math.asin,
-    "acos": math.acos,
-    "arccos": math.acos,
-    "atan": math.atan,
-    "arctan": math.atan,
-    "sqrt": math.sqrt,
-    "ln": math.log,
-    "lg": math.log10,
+    "sin": Func.sinFn,
+    "cosec": lambda x: 1 / Func.sinFn(x),
+    "csc": lambda x: 1 / Func.sinFn(x),
+    "cos": Func.cosFn,
+    "sec": lambda x: 1 / Func.cosFn(x),
+    "tan": Func.tanFn,
+    "cot": lambda x: 1 / Func.tanFn(x),
+    "sinh": Func.sinhFn,
+    "cosh": Func.coshFn,
+    "tanh": Func.tanhFn,
+    "asin": Func.asinFn,
+    "arcsin": Func.asinFn,
+    "acos": Func.acosFn,
+    "arccos": Func.acosFn,
+    "atan": Func.atanFn,
+    "arctan": Func.atanFn,
+    "sqrt": Func.sqrtFn,
+    "ln": Func.lnFn,
+    "lg": Func.lgFn,
     "normcdf": _normal.cdf,
     "normpdf": _normal.pdf,
     "invnorm": _normal.inv_cdf,
-    "acosh": math.acosh,
-    "asinh": math.asinh,
-    "atan2": math.atan2,
-    "atanh": math.atanh,
-    "ceil": math.ceil,
-    "comb": math.comb,
-    "copysign": math.copysign,
-    "degrees": math.degrees,
-    "dist": math.dist,
-    "erf": math.erf,
-    "erfc": math.erfc,
-    "exp": math.exp,
-    "expm1": math.expm1,
-    "fabs": math.fabs,
-    "factorial": math.factorial,
-    "floor": math.floor,
-    "fmod": math.fmod,
-    "frexp": math.frexp,
-    "fsum": math.fsum,
-    "gamma": math.gamma,
-    "gcd": math.gcd,
-    "hypot": math.hypot,
-    "isclose": math.isclose,
-    "isfinite": math.isfinite,
-    "isinf": math.isinf,
-    "isnan": math.isnan,
-    "isqrt": math.isqrt,
-    "lcm": math.lcm,
-    "ldexp": math.ldexp,
-    "lgamma": math.lgamma,
-    "log": math.log,
-    "log10": math.log10,
-    "log1p": math.log1p,
-    "log2": math.log2,
-    "modf": math.modf,
-    "nextafter": math.nextafter,
-    "perm": math.perm,
-    "pow": math.pow,
-    "prod": math.prod,
-    "radians": math.radians,
-    "remainder": math.remainder,
-    "trunc": math.trunc,
-    "ulp": math.ulp,
-    "cbrt": math.cbrt,
-    "exp2": math.exp2,
-    "sumprod": math.sumprod,
-    "fma": math.fma,
+    "acosh": Func.acoshFn,
+    "asinh": Func.asinhFn,
+    "atan2": Func.atan2Fn,
+    "atanh": Func.atanhFn,
+    "ceil": Func.ceilFn,
+    "comb": Func.combFn,
+    "copysign": Func.copysignFn,
+    "degrees": Func.degreesFn,
+    "dist": Func.distFn,
+    "erf": Func.erfFn,
+    "erfc": Func.erfcFn,
+    "exp": Func.expFn,
+    "expm1": Func.expm1Fn,
+    "fabs": Func.fabsFn,
+    "factorial": Func.factorialFn,
+    "floor": Func.floorFn,
+    "fmod": Func.fmodFn,
+    "frexp": Func.frexpFn,
+    "fsum": Func.fsumFn,
+    "gamma": Func.gammaFn,
+    "gcd": Func.gcdFn,
+    "hypot": Func.hypotFn,
+    "isclose": Func.iscloseFn,
+    "isfinite": Func.isfiniteFn,
+    "isinf": Func.isinfFn,
+    "isnan": Func.isnanFn,
+    "isqrt": Func.isqrtFn,
+    "lcm": Func.lcmFn,
+    "ldexp": Func.ldexpFn,
+    "lgamma": Func.lgammaFn,
+    "log": Func.logFn,
+    "log10": Func.log10Fn,
+    "log1p": Func.log1pFn,
+    "log2": Func.log2Fn,
+    "modf": Func.modfFn,
+    "nextafter": Func.nextafterFn,
+    "perm": Func.permFn,
+    "pow": Func.powFn,
+    "prod": Func.prodFn,
+    "radians": Func.radiansFn,
+    "remainder": Func.remainderFn,
+    "trunc": Func.truncFn,
+    "ulp": Func.ulpFn,
+    "cbrt": Func.cbrtFn,
+    "exp2": Func.exp2Fn,
+    "sumprod": Func.sumprodFn,
+    "fma": Func.fmaFn,
 }
 
 constants = {
@@ -92,16 +144,14 @@ constants = {
     "ten": 10.0,
     "half": 0.5,
     "onePointOne": 1.1,
-    "pi": math.pi,
-    "e": math.e,
-    "tau": math.tau,
-    "inf": math.inf,
-    "nan": math.nan,
-    "ln1_1": math.log(1.1),
-    "ln2": math.log(2.0),
-    "ln10": math.log(10.0),
-    "sqrt2": math.sqrt(2.0),
-    "sqrt_2pi": math.sqrt(2.0 * math.pi),
+    "pi": Func.PI,
+    "e": Func.E,
+    "tau": Func.TAU,
+    "ln1_1": Func.LN1_1,
+    "ln2": Func.LN2,
+    "ln10": Func.LN10,
+    "sqrt2": Func.SQRT2,
+    "sqrt_2pi": Func.SQRT_2PI,
     "imag_i": 1j,
 }
 
